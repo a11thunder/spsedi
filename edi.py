059d9a258d1ab536c8fac1f002ec249b7f807c2b
tequsta@gmail.com <tequesta@gmail.com>
2016-11-06 18:30:57 -0500

    Revamped code to look up store data from store.csv. Added functionality to cross reference invoiced POs and generate a second invoiced.csv.

--- edi.py
+++ edi.py
@@ -1,16 +1,33 @@
 import csv
+import os
 
+def ReadCSV(filename):
+	csvfile = open(filename, 'rb')
+	csvbuffer = csv.reader(csvfile)
+	datalist = []
+	for line in csvbuffer:
+		datalist.append(line)
+	csvfile.close()
+	headerlist = datalist[0]
+	datalist.pop(0)
+	return headerlist, datalist
+
+# If Wave invoice file exists, match with order list.
+Invoiced = False
+if os.path.isfile('invoice_items.csv'):
+	Invoiced = True
+
+# Stores lookup
+StoreLookup = False
+if os.path.isfile('stores.csv'):
+	StoreLookup = True
+	storehdrlist, storelist = ReadCSV('stores.csv')
+	colStoreLUID = storehdrlist.index('Store No.')
+	colStoreLUCity = storehdrlist.index('City')
+	colStoreLUState = storehdrlist.index('State')
 
 # Process SPS PO export file
-csvfile = open('documentDownloads-2016-10-27_17-38.csv', 'rb')
-csvbuffer = csv.reader(csvfile)
-datalist = []
-for line in csvbuffer:
-	datalist.append(line) 
-csvfile.close()
-
-headerlist = datalist[0]
-datalist.pop(0)
+headerlist, datalist = ReadCSV('documentDownloads-2016-10-27_17-38.csv')
 colRecordType = headerlist.index('Record Type')
 colPONum = headerlist.index('PO Number')
 colUPC = headerlist.index('UPC/EAN')
@@ -32,7 +49,7 @@ storeState = []
 for line in datalist:
 	if 'H' in line[colRecordType]:
 		orderlist.append(line[colPONum])
-		storeID.append(line[colStoreID])
+		storeID.append(int(line[colStoreID].strip()))
 		storeCity.append(line[colStoreCity])
 		storeState.append(line[colStoreState])
 		itemcount.append(0)
@@ -73,10 +90,42 @@ print
 print ' Total Orders: ' + str(len(orderlist))
 print
 
+if Invoiced:
+	headerlist, invoicelist = ReadCSV('invoice_items.csv')
+	colInvoiceNum = headerlist.index('invoice_num')
+	colInvoicePO = headerlist.index('po_so')
+	colInvoiceDate = headerlist.index('invoice_date')
+	colInvoiceDue = headerlist.index('due_date')
+	csvfile = open('invoiced.csv','wb')
+	csvwrite = csv.writer(csvfile)
+	csvbuffer = []
+	csvbuffer.append('Invoice Date')
+	csvbuffer.append('Due Date')
+	csvbuffer.append('PO Number')
+	csvbuffer.append('Invoice Number')
+	csvbuffer.append('Amount')
+	listprocessed = []
+	csvwrite.writerow(csvbuffer)
+	for line in invoicelist:
+		if line[colInvoicePO] in orderlist and line[colInvoicePO] not in listprocessed:
+			listprocessed.append(line[colInvoicePO].strip())
+			orderindex = orderlist.index(line[colInvoicePO].strip())
+			csvbuffer = []
+			csvbuffer.append(line[colInvoiceDate])
+			csvbuffer.append(line[colInvoiceDue])
+			csvbuffer.append(orderlist[orderindex])
+			csvbuffer.append(line[colInvoiceNum])
+			csvbuffer.append("%.2f" % round(ordertotal[orderindex],2))
+			csvwrite.writerow(csvbuffer)
+	csvfile.close()
+	print ' Total Order Invoiced: ' + str(len(listprocessed))
+	print
 
-csvfile = open('order.csv','wb')
+csvfile = open('orders.csv','wb')
 csvwrite = csv.writer(csvfile)
 csvbuffer = []
+if Invoiced:
+	csvbuffer.append('Status')
 csvbuffer.append('PO Number')
 for product in productname:
 	csvbuffer.append(product)
@@ -87,16 +136,23 @@ csvbuffer.append('State')
 csvwrite.writerow(csvbuffer)
 for iorder in range(len(orderlist)):
 	csvbuffer = []
+	if Invoiced:
+		if orderlist[iorder] in listprocessed:
+			csvbuffer.append('Invoiced')
+		else:
+			csvbuffer.append('---')
 	csvbuffer.append(orderlist[iorder])
 	for iprod in range(len(productlist)):
 		csvbuffer.append(str(itemizedqty[iorder][iprod]))
 	csvbuffer.append("%.2f" % round(ordertotal[iorder],2))
-	csvbuffer.append(storeID[iorder])
-	csvbuffer.append(storeCity[iorder])
-	csvbuffer.append(storeState[iorder])
+	csvbuffer.append(str(storeID[iorder]))
+	if StoreLookup:
+		for astore in storelist:
+			if storeID[iorder] == int(astore[colStoreLUID]):
+				csvbuffer.append(astore[colStoreLUCity])
+				csvbuffer.append(astore[colStoreLUState])
+	else:
+		csvbuffer.append(storeCity[iorder])
+		csvbuffer.append(storeState[iorder])
 	csvwrite.writerow(csvbuffer)
-
-csvfile.close()
-
-
-
+csvfile.close() 
